b81d709b3065a9d5a2ab011b3ffd0a0fed6bb139
smarky <smarkelon@gmail.com>
2017-06-26 11:58:00 -0400

    Complete purge before upload of first completed prototype

--- components/eiccomponents.py
+++
@@ -1,3 +0,0 @@
-target = "<detector name="/group/clas12/gemc/4a.1.0/experiments/clas12/targets/target" factory="TEXT" variation="lH2"/>"
-bst = "detector name="/group/clas12/gemc/4a.1.0/experiments/clas12/bst/bst"        factory="TEXT" variation="java"/>"
-cnd = "<detector name="/group/clas12/gemc/4a.1.0/experiments/clas12/cnd/cnd"        factory="TEXT" variation="original"/>"
