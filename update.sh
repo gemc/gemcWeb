b9abf60ca961ea82b2b4afd6fe157e853e59b81d
Sam A. Markelon <smarky@linuxmail.org>
2017-09-23 15:37:22 -0400

    update script revision

--- update.sh
+++ update.sh
@@ -9,6 +9,10 @@ export http_proxy=jprox.jlab.org:8081
 
 git pull
 
+rm -rf components
+rm -rf upload
+rm -rf users
+
 mv preserve_components components
 mv uploap_preserve upload
 mv users_preserve users
